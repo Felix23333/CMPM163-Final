17b0498c5824aefbfd7fe1571f08aa678860c27b
Felix Li <bli261@ucsc.edu>
2021-03-06 19:34:09 -0800

    Update Final Project

---
+++ Assets/Scripts/RestartParicle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RestartParicle : MonoBehaviour
+{
+    public float time = 14;
+    int count = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        count++;
+        if(count >= time * 50)
+        {
+            GetComponent<ParticleSystem>().Stop();
+            GetComponent<ParticleSystem>().Play();
+            count = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/animateTexture.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animateTexture : MonoBehaviour
+{
+    public float Xspeed = 0;
+    public float Yspeed = 0;
+    float curX;
+    float curY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        curX = GetComponent<MeshRenderer>().material.mainTextureOffset.x;
+        curY = GetComponent<MeshRenderer>().material.mainTextureOffset.y;
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        curX += Xspeed * Time.deltaTime;
+        curY += Yspeed * Time.deltaTime;
+        GetComponent<MeshRenderer>().material.SetTextureOffset("_MainTex", new Vector2(curX, curY));
+        GetComponent<MeshRenderer>().material.SetTextureOffset("_BumpMap", new Vector2(curX, curY));
+    }
+}
